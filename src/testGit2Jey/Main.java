49a45cd4c30c0b358e052f4866c00de27cd3840b
bradmanGitHub <mongkonchai_kk@hotmail.com>
2018-06-24 15:22:46 +0700

    fasd

--- src/testGit2Jey/Main.java
+++ src/testGit2Jey/Main.java
@@ -3,7 +3,7 @@ package testGit2Jey;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("test hanaga-local");
+		System.out.println("test hanaga-local-xx");
 		System.out.println("test hanaga-remote-22");
 	}
 
